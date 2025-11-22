8679d7ae3ffa07bc33e019d3676cc4415bda560c
17fastyyy <tonicorrea08@gmail.com>
2025-11-22 15:32:27 +0000

    Add comment functionality with model, forms, views, and templates

--- src/blogapp/views.py
+++ src/blogapp/views.py
@@ -3,8 +3,9 @@ from django.core.paginator import Paginator
 from .models import Post
 from django.http import Http404
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 class PostListView(ListView):
@@ -68,4 +69,37 @@ def post_share(request, post_id):
 
 def post_detail(request, post):
     post = get_object_or_404(Post, slug=post, status=Post.Status.PUBLISHED)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(
+        request,
+        'blog/post/detail.html',
+        {
+            'post': post,
+            'comments': comments,
+            'form': form
+            }
+    )
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id = post_id,
+        status = Post.Status.PUBLISHED
+    )
+    comment = None
+    form = CommentForm(data = request.POST)
+    if form.is_valid():
+        comment = form.save(commit = False)
+        comment.post = post
+        comment.save()
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment
+        }
+    )
